ed1c8ab32a672eab4a358371445a02870d04c496
Bowu Chen <1028505817@qq.com>
2021-11-19 16:27:34 +0800

    :art:

---
+++ DesignPattern.Builder/Builder/PhoneBuilder.cs
@@ -0,0 +1,39 @@
+﻿using DesignPattern.Builder.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern.Builder.Builder
+{
+    internal class PhoneBuilder : IPhoneBuilder
+    {
+        private IPhone _phone;
+        public IPhone Builder()
+        {
+            _phone = new ApplePhone();
+            throw new NotImplementedException();
+        }
+
+        public IPhoneBuilder UseCpu()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IPhoneBuilder UseMainboard()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IPhoneBuilder UseMemory()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IPhoneBuilder UseScreen()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DesignPattern.Builder/Interface/IPhone.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern.Builder.Interface
+{
+    internal interface IPhone
+    {
+        IMainboard Mainboard { get; set; }
+        ICpu Cpu { get; set; }
+        IMemory Memory { get; set; }
+        IScreen Screen { get; set; }
+        void Show();
+    }
+}
